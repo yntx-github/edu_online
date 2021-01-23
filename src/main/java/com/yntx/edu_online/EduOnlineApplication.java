1458ae22612ce4e1f6ba9a99b7e3d4d7ab0f14af
yntx-github <yntx-github@email.com>
2021-01-23 17:19:34 +0800

    init

---
+++ src/main/java/com/yntx/edu_online/EduOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.yntx.edu_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yntx/edu_online/EduOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yntx.edu_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
